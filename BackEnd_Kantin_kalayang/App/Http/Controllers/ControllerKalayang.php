d3a40c703b5968a9527832b6ad1e9df4cc721d10
Adriel Anderson Stefanus <adrielanderson.s@gmail.com>
2024-04-17 14:32:58 +0700

    baru dari el

--- BackEnd_Kantin_kalayang/App/Http/Controllers/ControllerKalayang.php
+++ BackEnd_Kantin_kalayang/App/Http/Controllers/ControllerKalayang.php
@@ -120,21 +120,24 @@ class ControllerKalayang extends Controller
         $id_penjual = $request->post('id_penjual');
         $nomor_meja = $request->post('nomor_meja');
         $alltransaksi = ModelKalayangTransaksi::select(
-                'id_menu',
-                DB::raw('MAX(id_order) AS id_order'),
-                DB::raw('MAX(tanggal_pemesanan) AS tanggal_pemesanan'),
-                DB::raw('MAX(nomor_meja) AS nomor_meja'),
-                DB::raw('MAX(status_pesanan) AS status_pesanan'),
-                DB::raw('MAX(catatan_pemesan) AS catatan_pemesan'),
-                DB::raw('MAX(ekstra_menu) AS ekstra_menu'),
-                DB::raw('MAX(created_at) AS created_at'),
-                DB::raw('MAX(updated_at) AS updated_at'),
-                DB::raw('COUNT(id_menu) AS Jumlah_pesan'),
-                'id_penjual'
+            'tb_transaksi.id_menu', 
+            DB::raw('MAX(tb_transaksi.id_order) AS id_order'),
+            DB::raw('MAX(tb_transaksi.tanggal_pemesanan) AS tanggal_pemesanan'),
+            DB::raw('MAX(tb_transaksi.nomor_meja) AS nomor_meja'),
+            DB::raw('MAX(tb_transaksi.status_pesanan) AS status_pesanan'),
+            DB::raw('MAX(tb_transaksi.catatan_pemesan) AS catatan_pemesan'),
+            DB::raw('MAX(tb_transaksi.ekstra_menu) AS ekstra_menu'),
+            DB::raw('MAX(tb_transaksi.created_at) AS created_at'),
+            DB::raw('MAX(tb_transaksi.updated_at) AS updated_at'),
+            DB::raw('COUNT(tb_transaksi.id_menu) AS Jumlah_pesan'),
+            'tb_transaksi.id_penjual',
+            DB::raw('SUM(tb_menu.harga_menu) AS harga_menu'),
             )
-            ->where('id_penjual', $id_penjual)
-            ->where('nomor_meja',  $nomor_meja)
-            ->groupBy('id_menu', 'id_penjual')
+            ->join('tb_menu', 'tb_menu.id_menu', '=', 'tb_transaksi.id_menu')
+            ->where('tb_transaksi.id_penjual', $id_penjual)
+            ->where('tb_transaksi.nomor_meja',  $nomor_meja)
+            ->whereNotIn('tb_transaksi.status_pesanan', ['SELESAI'])
+            ->groupBy('tb_transaksi.id_menu', 'tb_transaksi.id_penjual')
             ->get();
 
         return response()->json(['message' => 'success', 'data' => $alltransaksi], 200);
