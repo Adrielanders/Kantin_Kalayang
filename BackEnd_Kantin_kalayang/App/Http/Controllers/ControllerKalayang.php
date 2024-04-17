286dde6e2cb38838285ae2e8b017a07045f91d61
Adriel Anderson Stefanus <adrielanderson.s@gmail.com>
2024-04-17 17:04:33 +0700

    Push baru lagi

--- BackEnd_Kantin_kalayang/App/Http/Controllers/ControllerKalayang.php
+++ BackEnd_Kantin_kalayang/App/Http/Controllers/ControllerKalayang.php
@@ -122,10 +122,10 @@ class ControllerKalayang extends Controller
         $alltransaksi = ModelKalayangTransaksi::select(
             'tb_transaksi.id_menu', 
             DB::raw('MAX(tb_transaksi.id_order) AS id_order'),
-            DB::raw('MAX(tb_transaksi.tanggal_pemesanan) AS tanggal_pemesanan'),
+            DB::raw("DATE_FORMAT(MAX(tb_transaksi.tanggal_pemesanan), '%d/%m/%Y %h:%s') AS formatted_tanggal_pemesanan"),
             DB::raw('MAX(tb_transaksi.nomor_meja) AS nomor_meja'),
             DB::raw('MAX(tb_transaksi.status_pesanan) AS status_pesanan'),
-            DB::raw('MAX(tb_transaksi.catatan_pemesan) AS catatan_pemesan'),
+            DB::raw('MAX(tb_transaksi.catatan_pemesan) AS catatan_pemesan'),    
             DB::raw('MAX(tb_transaksi.ekstra_menu) AS ekstra_menu'),
             DB::raw('MAX(tb_transaksi.created_at) AS created_at'),
             DB::raw('MAX(tb_transaksi.updated_at) AS updated_at'),
@@ -172,6 +172,30 @@ class ControllerKalayang extends Controller
         }
 
         return response()->json(['message' => $msg, 'status' => $sts], 200);
+    }
+
+    public function viewrekap(Request $request){
+        $id_penjual = $request->post('id_penjual');
+        $alltransaksi = ModelKalayangTransaksi::select(
+            'tb_transaksi.id_menu', 
+            DB::raw('MAX(tb_transaksi.id_order) AS id_order'),
+            DB::raw("DATE_FORMAT(MAX(tb_transaksi.tanggal_pemesanan), '%d/%m/%Y %h:%s') AS formatted_tanggal_pemesanan"),
+            DB::raw('MAX(tb_transaksi.nomor_meja) AS nomor_meja'),
+            DB::raw('MAX(tb_transaksi.status_pesanan) AS status_pesanan'),
+            DB::raw('MAX(tb_transaksi.catatan_pemesan) AS catatan_pemesan'),    
+            DB::raw('MAX(tb_transaksi.ekstra_menu) AS ekstra_menu'),
+            DB::raw('MAX(tb_transaksi.created_at) AS created_at'),
+            DB::raw('MAX(tb_transaksi.updated_at) AS updated_at'),
+            DB::raw('COUNT(tb_transaksi.id_menu) AS Jumlah_pesan'),
+            'tb_transaksi.id_penjual',
+            DB::raw('SUM(tb_menu.harga_menu) AS harga_menu'),
+            )
+            ->join('tb_menu', 'tb_menu.id_menu', '=', 'tb_transaksi.id_menu')
+            ->where('tb_transaksi.id_penjual', $id_penjual)
+
+            ->whereNotIn('tb_transaksi.status_pesanan', ['SELESAI'])
+            ->groupBy('tb_transaksi.id_menu', 'tb_transaksi.id_penjual')
+            ->get();          
     }
 
 
