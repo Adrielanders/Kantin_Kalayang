de98f8e782c41cae56ba6a2cc4879ff5afdddd1d
Adriel Anderson Stefanus <adrielanderson.s@gmail.com>
2024-04-18 19:56:01 +0700

    ini buat jess kesayang

--- BackEnd_Kantin_kalayang/App/Models/ModelKalayangTransaksi.php
+++ BackEnd_Kantin_kalayang/App/Models/ModelKalayangTransaksi.php
@@ -10,5 +10,5 @@ class ModelKalayangTransaksi extends Model
     use HasFactory;
     protected $table ='tb_transaksi';
     protected $primaryKey ='id_transaksi ';
-    protected $fillable = ['id_menu', 'id_order','id_penjual','tanggal_pemesanan', 'nomor_meja','status_pesanan', 'catatan_pemesan', 'ekstra_menu','created_at','updated_at'];
+    protected $fillable = ['id_menu', 'id_order','id_penjual','tanggal_pemesanan', 'nomor_meja','status_pesanan', 'catatan_pemesan', 'ekstra_menu','pesanan','created_at','updated_at'];
 }
