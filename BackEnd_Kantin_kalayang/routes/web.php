297ba727a30bcaefcd7704daa59a1dfd39fe87a7
Adriel Anderson Stefanus <adrielanderson.s@gmail.com>
2024-04-19 15:38:54 +0700

    push baru dari el

--- BackEnd_Kantin_kalayang/routes/web.php
+++ BackEnd_Kantin_kalayang/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
+use App\Mail\SendEmail;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/testroute', function() {
+    $name = "Funny Coder";
+
+    Mail::to('testreceiver@gmail.com')->send(new SendEmail($name));
 });
