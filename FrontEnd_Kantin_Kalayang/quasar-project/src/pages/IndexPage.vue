410b8e09666df9473868ba105df3040fe7444a84
Adriel Anderson Stefanus <adrielanderson.s@gmail.com>
2024-05-07 16:06:08 +0700

    push baru lagi

--- FrontEnd_Kantin_Kalayang/quasar-project/src/pages/IndexPage.vue
+++ FrontEnd_Kantin_Kalayang/quasar-project/src/pages/IndexPage.vue
@@ -1,107 +1,19 @@
 <template>
-  <div id="app" class="bg-gradient">
-    <div class="black-line"></div>
-    <div class="center-box">
-      <div class="inner-circle"></div>
-      <div class="box">
-        <h6 id="fontjudul">Kantin Kalayang PUPR</h6>
-        <img src="/MapPin.png" >
-      </div>
-    </div>
-
-    <div class="top-half bg-gray"></div>
-    <div class="bottom-half bg-cream"></div>
-  </div>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+    <p>satu-kalayang</p>
+    <q-btn @click="$router.replace('/pengaturan-peran/')">sihd</q-btn>
+  </q-page>
 </template>
 
-<script setup>
-import { defineOptions } from "vue";
-defineOptions({
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
   name: "IndexPage",
 });
 </script>
-
-<style>
-#app {
-  font-family: Arial, sans-serif;
-  text-align: center;
-  min-height: 100vh; /* Menggunakan min-height agar konten tetap terlihat saat viewport kecil */
-  display: flex;
-  flex-direction: column;
-}
-
-.bg-gradient {
-  background: linear-gradient(to bottom, #d9d9d9 50%, #ffffff 50%);
-}
-
-.center-box {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: calc(100vh - 2px); /* Mengurangi tinggi garis hitam dari tinggi viewport */
-  position: relative; /* Ganti menjadi relative agar posisi kotak terkait dengan center-box */
-}
-
-.black-line {
-  position: absolute;
-  width: 100%;
-  height: 2px;
-  background-color: #000;
-  margin-top: calc(50vh - 1px); /* Menyesuaikan posisi tengah vertikal */
-  z-index: 1;
-}
-
-.box {
-  width: 294px; /* Lebar kotak */
-  height: 229px; /* Tinggi kotak */
-  background-color: #fff;
-  border: 1px solid #333;
-  border-radius: 5px;
-  padding: 20px;
-  z-index: 2; /* Mengatur kotak agar muncul di atas garis hitam */
-}
-
-.top-half,
-.bottom-half {
-  flex-grow: 1; /* Memastikan bagian atas dan bawah mengisi sisa ruang */
-}
-
-.bg-gray {
-  background-color: #d9d9d9; /* Warna abu-abu */
-}
-
-.bg-cream {
-  background-color: #ffffff; /* Warna krem */
-}
-
-.inner-circle {
-  position: absolute;
-  width: 130px;
-  height: 130px;
-  background-color: #9a9a9a;
-  border: 2px solid #000;
-  border-radius: 50%;
-  left: 50%;
-  transform: translateX(-50%);
-  z-index: 3;
-  margin-top: calc(-20vh - 65px); /* Menyesuaikan posisi tengah vertikal */
-}
-
-#fontjudul {
-  font-family: 'Inter', Arial, sans-serif;
-}
-
-/* Media queries untuk tampilan responsif */
-@media screen and (max-width: 700px) {
-  .box {
-    width: 80%; /* Mengubah lebar kotak menjadi responsif */
-    margin-top: 20px; /* Atur margin atas untuk jarak dari lingkaran */
-  }
-
-  .inner-circle {
-    width: 100px; /* Mengubah lebar lingkaran dalam kotak */
-    height: 100px; 
-    margin-top: calc(50vh - 595px);
-  }
-}
-</style>
---
+++ FrontEnd_Kantin_Kalayang/quasar-project/src/pages/page-pembeli/HomePembeli.vue
@@ -0,0 +1,224 @@
+import { route } from 'quasar/wrappers';
+<template>
+  <!-- <q-btn flat @click="$router.replace('/')" icon="arrow_back" style="margin-top: 5px;"></q-btn> -->
+  <HeaderCreate title="SATU KALAYANG" backAction="/" :hideLogout="true" :hideProfile="true"/>
+
+  <div style="display: flex; justify-content: center; height: 100vh">
+    <q-page style="display: flex; flex-direction: column; height: 100vh">
+      <div style="display: flex; flex-direction: column; align-items: center">
+        <!-- <h6 style="font-weight: 800; margin: 0%">SATU KALAYANG</h6> -->
+
+        <q-input
+          outlined
+          class="search-bar"
+          v-model="text"
+          placeholder="Cari makanan / toko"
+          color="black"
+          style="padding-top: 10px; width: 93%"
+        >
+          <!-- <q-icon name="event" color="orange" /> -->
+          <q-btn
+            unelevated
+            size="13px"
+            no-caps
+            class="text-weight-medium"
+            icon="search"
+            style="padding: 0%"
+          />
+        </q-input>
+      </div>
+      <div class="categories" style="display: flex">
+        <div style="text-align: center; margin: auto">
+          <img
+            src="/src/assets/WhatsApp Image 2024-04-30 at 13.17.28_a0a48e8c.jpg"
+            alt="Deskripsi Foto"
+            class="category"
+          />
+          <p class="text-category">Makanan Berat</p>
+        </div>
+        <div style="text-align: center; margin: auto">
+          <img
+            src="/src/assets/WhatsApp Image 2024-04-30 at 13.17.28_a0a48e8c.jpg"
+            alt="Deskripsi Foto"
+            class="category"
+          />
+          <p class="text-category">Makanan Ringan</p>
+        </div>
+        <div style="text-align: center; margin: auto">
+          <img
+            src="/src/assets/WhatsApp Image 2024-04-30 at 13.17.28_a0a48e8c.jpg"
+            alt="Deskripsi Foto"
+            class="category"
+          />
+          <p class="text-category">Minuman</p>
+        </div>
+      </div>
+
+      <!-- display: flex; -->
+      <div
+        style="flex-direction: column; margin-left: 12px; margin-right: 12px"
+      >
+        <div>
+          <h6 style="font-weight: 800; margin-top: 5px; margin-bottom: 5px">
+            Diskon Hari Ini
+          </h6>
+          <!-- autoplay
+            @mouseenter="autoplay1 = false"
+            @mouseleave="autoplay1 = true" -->
+          <q-carousel
+            arrows
+            animated
+            infinite
+            v-model="slide1"
+            class="carousel"
+            style="width: 40%;"
+          >
+            <q-carousel-slide name="first" class="carousel-slide">
+              <q-img
+                class="pesan-img"
+                src="https://cdn.quasar.dev/img/mountains.jpg"
+              />
+              <div class="text-container">
+                <div class="text-h6">Kios 1</div>
+                <div class="text-subtitle1">
+                  Deskripsi kios
+                </div>
+              </div>
+            </q-carousel-slide>
+            <q-carousel-slide name="second" class="carousel-slide">
+              <q-img
+                class="pesan-img"
+                src="https://cdn.quasar.dev/img/parallax1.jpg"
+              />
+              <div class="text-container">
+                <div class="text-h6">Kios 2</div>
+                <div class="text-subtitle1">
+                  Deskripsi kios ini jual apa aja lalalalala
+                </div>
+              </div>
+            </q-carousel-slide>
+          </q-carousel>
+        </div>
+        <div>
+          <h6 style="font-weight: 800; margin-top: 20px; margin-bottom: 5px">
+            Pesan Sekarang
+          </h6>
+          <!-- autoplay
+            @mouseenter="autoplay2 = false"
+            @mouseleave="autoplay2 = true" -->
+          <q-carousel
+            arrows
+            animated
+            infinite
+            v-model="slide2"
+            class="carousel"
+          >
+            <q-carousel-slide name="first" class="carousel-slide">
+              <q-img
+                class="pesan-img"
+                src="https://cdn.quasar.dev/img/mountains.jpg"
+              />
+              <div class="text-container">
+                <div class="text-h6">Kios 1</div>
+                <div class="text-subtitle1">
+                  Deskripsi kios ini jual apa aja lalalalala
+                </div>
+              </div>
+            </q-carousel-slide>
+            <q-carousel-slide name="second" class="carousel-slide">
+              <q-img
+                class="pesan-img"
+                src="https://cdn.quasar.dev/img/parallax1.jpg"
+              />
+              <div class="text-container">
+                <div class="text-h6">Kios 2</div>
+                <div class="text-subtitle1">
+                  Deskripsi kios ini jual apa aja lalalalala
+                </div>
+              </div>
+            </q-carousel-slide>
+          </q-carousel>
+        </div>
+      </div>
+    </q-page>
+  </div>
+</template>
+
+<script>
+import HeaderCreate from "components/HeaderCreate.vue";
+import { ref } from "vue";
+
+export default {
+  components: {
+    HeaderCreate,
+  },
+  setup() {
+    const text = ref("");
+
+    const autoplay1 = ref(true);
+    const autoplay2 = ref(true);
+
+    return {
+      text,
+
+      slide1: ref("first"),
+      slide2: ref("first"),
+      autoplay1,
+      autoplay2,
+    };
+  },
+};
+</script>
+
+<style>
+@media screen and (max-width: 40vh) {
+  .categories {
+    display: flex;
+    flex-direction: column;
+  }
+}
+
+.category {
+  max-width: 80%;
+  height: auto;
+  margin: auto;
+  margin-top: 20px;
+  margin-bottom: 5px;
+}
+
+.text-category {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.carousel {
+  height: 50%;
+}
+
+.carousel-slide {
+  position: relative;
+  width: 100%;
+  height: 50%;
+  padding: 0%;
+  overflow: hidden;
+}
+
+.text-container {
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  text-align: center;
+}
+
+.pesan-img {
+  object-fit: cover;
+  width: 100%;
+  /* height: 40%; */
+  height: 150px;
+}
+</style>
